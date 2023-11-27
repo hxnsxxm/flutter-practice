964489c8f7690a32cc1c40dc46c5a5c92d8726f1
hansaem <hansaemj20@gmail.com>
2023-11-27 10:42:23 +0900

    예제/앱의 레이아웃 만드는 법(Scaffold)

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -4,11 +4,35 @@ void main() {
   runApp(const MyApp());
 }
 
+// 앱의 메인페이지
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("앱임"),
+          centerTitle: false,
+        ),
+        body: Container(
+          child: Text('안녕')
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
+            )
+          )
+        )
+      )
+    );
   }
 }
