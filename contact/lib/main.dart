530ac8c638f4395f1d2ff13cf9ff442f7dc4d32c
hansaem <hansaemj20@gmail.com>
2023-11-28 14:35:34 +0900

    stateful, button

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 // 앱의 메인페이지
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            setState(() {
+              a++;
+            });
+          },
+        ),
         appBar: AppBar(),
         body: ListView.builder(
           itemCount: 3, // 반복 횟수
           itemBuilder: (context, i) { // 함수
-            print(i); // console에 출력
             return ListTile(
                 leading: Icon(Icons.account_circle),
                 title: Text('홍길동')
