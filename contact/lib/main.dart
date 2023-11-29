228e3e5c1366c9ca3557b76d6590a4ed3d9305ea
hansaem <hansaemj20@gmail.com>
2023-11-29 10:51:04 +0900

    예제/StatefulWidget 만들기

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -14,26 +14,28 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var a = 1;
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Text(a.toString()),
-          onPressed: (){
-            setState(() {
-              a++;
-            });
-          },
-        ),
         appBar: AppBar(),
         body: ListView.builder(
           itemCount: 3, // 반복 횟수
           itemBuilder: (context, i) { // 함수
             return ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('홍길동')
+              leading: Text(like[i].toString()),
+              title: Text(name[i]),
+              trailing: ElevatedButton(
+                child: Text('좋아요'),
+                onPressed: () {
+                  setState(() {
+                    like[i]++;
+                  });
+                },
+              ),
             );
           },
         ),
