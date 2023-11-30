9a4d0658bd0f11508e42e29f58ce27910dd7e38d
hansaem <hansaemj20@gmail.com>
2023-11-30 15:40:06 +0900

    예제/자식 위젯이 부모 위젯의 state를 변경하고 싶다면

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      home:MyApp()
+    )
+  );
 }
 
 // 앱의 메인페이지
@@ -13,52 +16,76 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var total = 3;
   var name = ['김영숙', '홍길동', '피자집'];
   var like = [0, 0, 0];
 
+  addName(a) {
+    setState(() {
+      name.add(a);
+    });
+  }
+
+  addOne() {
+    setState(() {
+      total++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: ListView.builder(
-          itemCount: 3, // 반복 횟수
-          itemBuilder: (context, i) { // 함수
-            return ListTile(
-              leading: Text(like[i].toString()),
-              title: Text(name[i]),
-              trailing: ElevatedButton(
-                child: Text('좋아요'),
-                onPressed: () {
-                  setState(() {
-                    like[i]++;
-                  });
-                },
-              ),
-            );
-          },
-        ),
-        bottomNavigationBar: CustomBottomBar(),
-      )
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(context: context, builder: (context){
+            return DialogUI(addOne : addOne, addName : addName);
+          });
+        },
+      ),
+      appBar: AppBar(title: Text(total.toString())),
+      body: ListView.builder(
+        itemCount: name.length, // 반복 횟수
+        itemBuilder: (context, i) { // 함수
+          return ListTile(
+            leading: Image.asset('assets/icon_profile01.png'),
+            title: Text(name[i]),
+          );
+        },
+      ),
     );
   }
 }
 
-class CustomBottomBar extends StatelessWidget {
-  const CustomBottomBar({super.key});
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.addOne, this.addName}) : super(key: key);
+  var addOne;
+  var addName;
+  var inputData = TextEditingController();
+  var inputData2 = '';
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      height: 60,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.phone),
-          Icon(Icons.message),
-          Icon(Icons.contact_page),
-        ],
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(
+              //onChanged: (text){ inputData2 = text; }, // 사용자가 입력한 값
+              controller: inputData,
+            ),
+            TextButton( child: Text('완료'), onPressed:(){
+              addOne();
+              //addName(inputData2);
+              addName(inputData.text);
+              Navigator.pop(context);
+            } ), // state.toString()
+            TextButton(
+                child: Text('취소'),
+                onPressed:(){ Navigator.pop(context); })
+          ],
+        ),
       ),
     );
   }
